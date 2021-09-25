bc101f36ef121c841c34b5f367ac3efee6fb5d3c
Bjørn Thorlacius <bjorn.thorlacius@knowit.no>
2021-09-25 12:41:09 +0200

    RN-camera initialized and working

--- App.js
+++ App.js
@@ -2,15 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import MainNavigator from "./navigation/MainNavigator";
-import NodeCamera from "./components/NodeCamera";
+// import NodeCamera from "./components/NodeCamera";
 
 import theme from "./navigation/theme";
+import CameraViewExample from "./CameraViewExample";
 
 export default function App(props) {
   return (
-    <NavigationContainer theme={theme}>
-      <MainNavigator />
-    </NavigationContainer>
-    // <NodeCamera />
+    // <NavigationContainer theme={theme}>
+    //   <MainNavigator />
+    // </NavigationContainer>
+    <CameraViewExample />
   );
 }
